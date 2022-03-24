0ba2d8c852159072d4bde22883fc2bba27c5d601
srihankunde <90851633+srihankunde@users.noreply.github.com>
2022-03-24 16:59:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,4 +25,33 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function model_loaded(){
 
     console.log("MODEL IS LOADED!");
+
+
+
 }
+
+
+
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img , gotResult);
+    
+    
+    }
+    
+    function gotResult(error, results){
+    
+    if (error){
+    
+       console.error(error);
+    
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_name").innerHTML=results[0].label;
+        document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    
+    }
+    
+    }
